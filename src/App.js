8645308d633abef6534b4719b269f8746856fda1
thatGui <gui@thatgui.com>
2022-03-02 17:51:20 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import foods from './foods.json';
+import React, { useState } from 'react';
+import { Row, Divider, Button } from 'antd';
+import FoodBox from './components/foodBox/FoodBox';
+import AddFoodForm from './components/foodForm/addFoodForm';
 
 function App() {
+  const [dishes, setDishes] = useState(foods);
+
+  const addFood = (newDishes) => {
+    const updateddishes = [...dishes, newDishes];
+    setDishes(updateddishes);
+    console.log(updateddishes);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AddFoodForm newFood={addFood}/>
+
+      <Button> Hide Form / Add New Food </Button>
+
+      {/* Display Search component here */}
+
+      <Divider>Food List</Divider>
+
+      <Row style={{ width: '100%', justifyContent: 'center' }}>
+        {dishes.map((el) => <FoodBox food={el} />)}
+      </Row>
     </div>
   );
 }
---
+++ src/components/foodForm/addFoodForm.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { Divider, Input } from 'antd';
+
+// Iteration 4
+function AddFoodForm(props) {
+    
+    const [name, setName] = useState('');
+    const [calories, setCalories] = useState(0);
+    const [image, setImage] = useState('');
+    const [servings, setServings] = useState(0);
+
+  const handleNameInput = (e) => setName(e.target.value);
+  const handleCaloriesInput = (e) => setCalories(e.target.value);
+  const handleImageInput = (e) => setImage(e.target.value);
+  const handleServingsInput = (e) => setServings(e.target.value);
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    const newItem = {name, calories, image, servings};
+    props.addFood(newItem);
+};
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Divider>Add Food Entry</Divider>
+
+      <label>Name</label>
+      <Input value={name} type="text" onChange={(e) => {handleNameInput(e)}} />
+
+      <label>Calories</label>
+      <Input value={calories} type="number" onChange={(e) => {handleCaloriesInput(e)}} />
+
+
+      <label>Image</label>
+      <Input value={image} type="text" onChange={(e) => {handleImageInput(e)}} />
+      
+
+      <label>Servings</label>
+      <Input value={servings} type="text" onChange={(e) => {handleServingsInput(e)}} />
+      
+
+      <button type="submit">Create</button>
+    </form>
+  );
+}
+
+export default AddFoodForm;
